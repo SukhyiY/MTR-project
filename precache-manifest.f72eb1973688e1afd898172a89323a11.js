aaf051fe07329b972669f50d0608eae95b99ed66
SukhyiY <voyarko@gmail.com>
2019-10-30 17:25:57 +0200

    Updates

---
+++ precache-manifest.f72eb1973688e1afd898172a89323a11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b362ca002d96c43b45e38ecdc87d58aa",
+    "url": "/MTR-project/index.html"
+  },
+  {
+    "revision": "5ed70f8558481299bddf",
+    "url": "/MTR-project/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "cfe0d189be9dff715630",
+    "url": "/MTR-project/static/js/2.3dd157b3.chunk.js"
+  },
+  {
+    "revision": "5ed70f8558481299bddf",
+    "url": "/MTR-project/static/js/main.37778eac.chunk.js"
+  },
+  {
+    "revision": "23a1240283047bf16221",
+    "url": "/MTR-project/static/js/runtime-main.270e556b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MTR-project/precache-manifest.87e44df06c60b24f8ad1fd16b5d2f435.js"
+  "/MTR-project/precache-manifest.f72eb1973688e1afd898172a89323a11.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/MTR-project/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MTR-project/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
